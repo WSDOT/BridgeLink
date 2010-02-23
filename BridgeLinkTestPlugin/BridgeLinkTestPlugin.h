693b2d67730c01e3259af4d6b095e7d75fcc0eba
WSDOTbricer <WSDOTbricer>
2010-02-23 00:56:52 +0000

    *** empty log message ***

--- BridgeLinkTestPlugin/BridgeLinkTestPlugin.h
+++ BridgeLinkTestPlugin/BridgeLinkTestPlugin.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Fri Feb 19 18:17:47 2010
+/* at Mon Feb 22 16:17:40 2010
  */
 /* Compiler settings for .\BridgeLinkTestPlugin.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
