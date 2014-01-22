f4c8284fea346a31f592aa1ce19042103e4a6c1b
WSDOTbricer <WSDOTbricer>
2014-01-22 21:10:40 +0000

    *** empty log message ***

--- ScreenSizeDlg.h
+++ ScreenSizeDlg.h
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////
 // BridgeLink
-// Copyright © 1999-2013  Washington State Department of Transportation
+// Copyright © 1999-2014  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This program is free software; you can redistribute it and/or modify
