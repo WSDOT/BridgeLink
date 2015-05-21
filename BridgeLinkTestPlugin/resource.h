4982176e0bad5432dffad83728481df9bcd7e02f
WSDOTbricer <WSDOTbricer>
2015-05-21 03:23:11 +0000

    *** empty log message ***

--- BridgeLinkTestPlugin/resource.h
+++ BridgeLinkTestPlugin/resource.h
@@ -8,14 +8,14 @@
 #define IDR_COMPONENTINFO               130
 #define IDI_ICON1                       205
 #define IDI_ICON                        205
-#define ID_NEW_VIEW                     32768
+#define ID_NEW_VIEW                     33029
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        206
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_COMMAND_VALUE         33030
 #define _APS_NEXT_CONTROL_VALUE         201
 #define _APS_NEXT_SYMED_VALUE           103
 #endif
