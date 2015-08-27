55f51fc635696b8dc3cee3c0341202a805393c50
WSDOTbricer <WSDOTbricer>
2015-08-27 18:08:50 +0000

    *** empty log message ***

--- Installer/Scripts/BuildPGSuperImagesFragment.js
+++ Installer/Scripts/BuildPGSuperImagesFragment.js
@@ -1,6 +1,6 @@
 // JScript source code
 var FSO = new ActiveXObject("Scripting.FileSystemObject");
-var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\PGSuper\\3.0.0.38\\bin\\images");
+var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\PGSuper\\3.0.0.39\\bin\\images");
 var fc = new Enumerator(ImagesFolder.Files);
 
 WScript.Echo("<?xml version='1.0'?>");
