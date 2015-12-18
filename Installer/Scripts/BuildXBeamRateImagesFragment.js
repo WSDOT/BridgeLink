b165a3f81c487e65d4c081e41e1f26a6cfd494da
WSDOTbricer <WSDOTbricer>
2015-12-18 16:53:21 +0000

    *** empty log message ***

--- Installer/Scripts/BuildXBeamRateImagesFragment.js
+++ Installer/Scripts/BuildXBeamRateImagesFragment.js
@@ -1,6 +1,6 @@
 // JScript source code
 var FSO = new ActiveXObject("Scripting.FileSystemObject");
-var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\XBeamRate\\1.0.0.2\\bin\\images");
+var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\XBeamRate\\1.0.0.3\\bin\\images");
 var fc = new Enumerator(ImagesFolder.Files);
 
 WScript.Echo("<?xml version='1.0'?>");
