dcb3946b956157b1bab11316a1489c25357a978b
WSDOTBriceR <WSDOTBriceR>
2017-12-20 16:50:31 +0000

    Updated copyright for 2018

--- BridgeLinkTestPlugin/ComponentInfo.cpp
+++ BridgeLinkTestPlugin/ComponentInfo.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////
 // BridgeLink Example - BridgeLink Application Framework Example
-// Copyright © 1999-2017  Washington State Department of Transportation
+// Copyright © 1999-2018  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This program is free software; you can redistribute it and/or modify
