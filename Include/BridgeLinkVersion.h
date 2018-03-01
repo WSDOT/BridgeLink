a2723ec5b4bb0733b3be14f9b3f7842ff9baf41a
WSDOTBriceR <WSDOTBriceR>
2018-03-01 20:42:15 +0000

    *** empty log message ***

--- Include/BridgeLinkVersion.h
+++ Include/BridgeLinkVersion.h
@@ -26,7 +26,7 @@
 #define _BRIDGELINK_VERSION 114 // version 1.1.4
 #endif
 
-#define _BETA_VERSION          // When defined, building beta versions
+//#define _BETA_VERSION          // When defined, building beta versions
 
 // includes WBFL Version macro and
 // macros that define the minimum version of Windows and IE
