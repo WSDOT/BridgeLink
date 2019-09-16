45ff6a613422ce1086a35a0d34f703f5b3bdf993
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2019-09-16 12:35:54 -0700

    Updated version numbers for BridgeLink 5.0.0 beta 3

--- Installer/Scripts/BuildPGSuperFragments.js
+++ Installer/Scripts/BuildPGSuperFragments.js
@@ -1,6 +1,6 @@
 // JScript source code
 var FSO = new ActiveXObject("Scripting.FileSystemObject");
-var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\PGSuper\\5.0.0.2\\bin\\images");
+var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\PGSuper\\5.0.0.3\\bin\\images");
 var PGSuperDocsFolder = FSO.GetFolder("\\ARP\\BridgeLink\\Docs\\PGSuper\\5.0");
 var PGSpliceDocsFolder = FSO.GetFolder("\\ARP\\BridgeLink\\Docs\\PGSplice\\5.0");
 var PGSLibraryDocsFolder = FSO.GetFolder("\\ARP\\BridgeLink\\Docs\\PGSLibrary\\5.0");
