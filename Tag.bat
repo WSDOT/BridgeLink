7f0308946387aa867955f9796d67d4f1690d1549
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2020-07-28 12:35:18 -0700

    Adds script to tag and push all the BridgeLink repositories

---
+++ Tag.bat
@@ -0,0 +1,42 @@
+REM - Script to tag all the git repositories
+
+REM - This is the tag that will be applied to the repository
+REM - for official releases, use x.y.z version number
+REM - for beta releases, use x.y.z_beta_n 
+SET TAG=version_5.0.4.0
+
+cd \ARP\BEToolbox\
+git tag %TAG%
+git push
+git push --tags
+
+cd \ARP\BEToolboxExtensions\
+git tag %TAG%
+git push
+git push --tags
+
+cd \ARP\Barlist\
+git tag %TAG%
+git push
+git push --tags
+
+cd \ARP\PGSuper\
+git tag %TAG%
+git push
+git push --tags
+
+cd \ARP\XBRate\
+git tag %TAG%
+git push
+git push --tags
+
+cd \ARP\WBFL\
+git tag %TAG%
+git push
+git push --tags
+
+cd \ARP\BridgeLink\
+git tag %TAG%
+git push
+git push --tags
+
