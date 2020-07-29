027071ff6a59e3b40720d2c886ed0469bc597081
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2020-07-29 11:17:08 -0700

    Updated version numbers for 6.0 Beta 1

--- Installer/Scripts/BuildXBeamRateFragments.js
+++ Installer/Scripts/BuildXBeamRateFragments.js
@@ -1,6 +1,6 @@
 // JScript source code
 var FSO = new ActiveXObject("Scripting.FileSystemObject");
-var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\XBeamRate\\6.0.0.0\\bin\\images");
+var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\XBeamRate\\6.0.0.1\\bin\\images");
 var XBRateDocsFolder = FSO.GetFolder("\\ARP\\BridgeLink\\Docs\\XBRate\\6.0");
 var fc = new Enumerator(ImagesFolder.Files);
 
--- Tag.bat
+++ Tag.bat
@@ -3,33 +3,40 @@ REM - Script to tag all the git repositories
 REM - This is the tag that will be applied to the repository
 REM - for official releases, use x.y.z version number
 REM - for beta releases, use x.y.z_beta_n 
-SET TAG=6.0.0_beta_0
+SET TAG=6.0.0_beta_1
 
 cd \ARP\BEToolbox\
 git tag %TAG%
+git push
 git push --tags
 
 cd \ARP\BEToolboxExtensions\
 git tag %TAG%
+git push
 git push --tags
 
 cd \ARP\Barlist\
 git tag %TAG%
+git push
 git push --tags
 
 cd \ARP\PGSuper\
 git tag %TAG%
+git push
 git push --tags
 
 cd \ARP\XBRate\
 git tag %TAG%
+git push
 git push --tags
 
 cd \ARP\WBFL\
 git tag %TAG%
+git push
 git push --tags
 
 cd \ARP\BridgeLink\
 git tag %TAG%
+git push
 git push --tags
 
