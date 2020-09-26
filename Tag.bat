3ef8cf62f6ab58bf88afe4ae252310d942fecb89
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2020-09-26 08:07:11 -0700

    Updated version numbers for 6.0 Beta 2

--- Tag.bat
+++ Tag.bat
@@ -3,40 +3,41 @@ REM - Script to tag all the git repositories
 REM - This is the tag that will be applied to the repository
 REM - for official releases, use x.y.z version number
 REM - for beta releases, use x.y.z_beta_n 
-SET TAG=6.0.0_beta_1
+REM SET TAG=6.0.0_beta_2
+REM git tag %TAG% isn't working so the takes are explicit... use search/replace to update in all places
 
 cd \ARP\BEToolbox\
-git tag %TAG%
+git tag 6.0.0_beta_2
 git push
 git push --tags
 
 cd \ARP\BEToolboxExtensions\
-git tag %TAG%
+git tag 6.0.0_beta_2
 git push
 git push --tags
 
 cd \ARP\Barlist\
-git tag %TAG%
+git tag 6.0.0_beta_2
 git push
 git push --tags
 
 cd \ARP\PGSuper\
-git tag %TAG%
+git tag 6.0.0_beta_2
 git push
 git push --tags
 
 cd \ARP\XBRate\
-git tag %TAG%
+git tag 6.0.0_beta_2
 git push
 git push --tags
 
 cd \ARP\WBFL\
-git tag %TAG%
+git tag 6.0.0_beta_2
 git push
 git push --tags
 
 cd \ARP\BridgeLink\
-git tag %TAG%
+git tag 6.0.0_beta_2
 git push
 git push --tags
 
