b563639766cfb461417cf867bec9472d028627ce
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2022-04-18 08:12:56 -0700

    Updated copyright year

--- BridgeLink.cpp
+++ BridgeLink.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////
 // BridgeLink - BridgeLink Extensible Application Framework
-// Copyright © 1999-2021  Washington State Department of Transportation
+// Copyright © 1999-2022  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This program is free software; you can redistribute it and/or modify
