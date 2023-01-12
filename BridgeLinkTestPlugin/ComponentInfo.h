31fb3f277bf66348c37eb4afbaa1c6179a792ca0
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2023-01-12 12:36:16 -0800

    Updated copyright to 2023

--- BridgeLinkTestPlugin/ComponentInfo.h
+++ BridgeLinkTestPlugin/ComponentInfo.h
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////
 // BridgeLink Example - BridgeLink Application Framework Example
-// Copyright © 1999-2022  Washington State Department of Transportation
+// Copyright © 1999-2023  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This program is free software; you can redistribute it and/or modify
