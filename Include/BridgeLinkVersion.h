a7d3e1da232ba5d2c81ddb5d143416c5d81af802
Richard Brice <bricer@wsdot.wa.gov>
2024-01-17 09:32:28 -0800

    Updates copyright year

--- Include/BridgeLinkVersion.h
+++ Include/BridgeLinkVersion.h
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////
 // BridgeLink - BridgeLink Extensible Application Framework
-// Copyright © 1999-2023  Washington State Department of Transportation
+// Copyright © 1999-2024  Washington State Department of Transportation
 //                        Bridge and Structures Office
 //
 // This program is free software; you can redistribute it and/or modify
