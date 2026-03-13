e9ce853c53f6d743c9cd0c9b50ec38f53a38fcbe
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2026-03-13 07:39:54 -0700

    Clean up before building installer

--- Installer/Scripts/BuildBEToolboxFragments.js
+++ Installer/Scripts/BuildBEToolboxFragments.js
@@ -1,6 +1,6 @@
 // JScript source code
 var FSO = new ActiveXObject("Scripting.FileSystemObject");
-var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\BEToolbox\\9.0.0.1\\bin\\images");
+var ImagesFolder = FSO.GetFolder("\\ARP_RELEASE\\BEToolbox\\9.0.0.2\\bin\\images");
 var BEToolboxDocsFolder = FSO.GetFolder("\\ARP\\BridgeLink\\Docs\\BEToolbox\\9.0");
 var BEToolboxDocsSearchFolder = FSO.GetFolder("\\ARP\\BridgeLink\\Docs\\BEToolbox\\9.0\\search");
 var fc = new Enumerator(ImagesFolder.Files);
